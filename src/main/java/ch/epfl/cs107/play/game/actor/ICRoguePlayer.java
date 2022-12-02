10d7c47e29986779eb2cf1462af32216c49a6a02
davidfota <fotadavid23@yahoo.com>
2022-12-02 14:50:24 +0100

    1

---
+++ src/main/java/ch/epfl/cs107/play/game/actor/ICRoguePlayer.java
@@ -0,0 +1,7 @@
+package ch.epfl.cs107.play.game.actor;
+
+public class ICRoguePlayer {
+    public String getTitle(){
+        return "ICRoguePlayer";
+    }
+}
---
+++ src/main/java/ch/epfl/cs107/play/game/icrogue/area/level10/rooms/Level10Room.java
@@ -0,0 +1,7 @@
+package ch.epfl.cs107.play.game.icrogue.area.level10.rooms;
+
+import ch.epfl.cs107.play.game.icrogue.area.ICRogueRoom;
+
+public class Level10Room extends ICRogueRoom {
+
+}
