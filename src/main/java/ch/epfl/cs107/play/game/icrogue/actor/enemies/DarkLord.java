3f0f3f3c2d4aaadd9769d823e0ca21a45611343f
fotadavid <114075687+fotadavid@users.noreply.github.com>
2022-12-21 14:55:38 +0100

    1

--- src/main/java/ch/epfl/cs107/play/game/icrogue/actor/enemies/DarkLord.java
+++ src/main/java/ch/epfl/cs107/play/game/icrogue/actor/enemies/DarkLord.java
@@ -4,13 +4,16 @@ import ch.epfl.cs107.play.game.areagame.Area;
 import ch.epfl.cs107.play.game.areagame.actor.Animation;
 import ch.epfl.cs107.play.game.areagame.actor.Orientation;
 import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.game.icrogue.RandomHelper;
 import ch.epfl.cs107.play.game.icrogue.actor.ICRogueActor;
+import ch.epfl.cs107.play.game.icrogue.actor.projectiles.Fire;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.math.RegionOfInterest;
 import ch.epfl.cs107.play.math.Vector;
 import ch.epfl.cs107.play.window.Canvas;
 
 import java.nio.channels.spi.SelectorProvider;
+import java.util.Random;
 
 public class DarkLord extends ICRogueActor {
     private final Sprite[] sprites = new Sprite[4];
@@ -30,13 +33,24 @@ public class DarkLord extends ICRogueActor {
                         new RegionOfInterest(i * 32, j*32, 32, 32));
         currentSprite = sprites[0];
         currentSpriteLine = spellSprites[0];
-        spellAnimation = new Animation(24, sprites);
+        spellAnimation = new Animation(24, currentSpriteLine);
         spellAnimation.setSpeedFactor(12);
         spellAnimation.setAnchor(new Vector(0, 0));
         spellAnimation.setHeight(1f);
         spellAnimation.setHeight(2f);
     }
-
+    public void update()
+    private void moveToRandomPosition()
+    {
+        int x = RandomHelper.roomGenerator.nextInt(1, 9);
+        int y = RandomHelper.roomGenerator.nextInt(1, 9);
+        DiscreteCoordinates randomPosition = new DiscreteCoordinates(x, y);
+        changePosition(randomPosition);
+    }
+    public void attack(){
+        getOwnerArea().registerActor(new Fire(getOwnerArea(), getOrientation(), getCurrentMainCellCoordinates()));
+        isAttacking = false;
+    }
     @Override
     public boolean draw(Canvas canvas) {
         if(isAttacking)
